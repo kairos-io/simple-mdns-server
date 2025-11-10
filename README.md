7a5de2696f14e73b63d1514aa6836c567d8ec0d1
Dimitris Karakasilis <dimitris@karakasilis.me>
2025-11-10 14:41:54 +0200

    Add "lookup" command to debug the server using the same binary
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-This is a simple utility that takes 4 arguments:
+This is a simple utility that can operate in two modes:
+
+## Server Mode (default)
+
+Advertises a service over mDNS. Takes 4 arguments:
 
 - a network interface or an IP address
 - a port
@@ -24,6 +28,31 @@ or
 ```
 go run . --port 8000 --address 192.168.1.100 --serviceType _kcrypt._tcp --hostName myserver.local
 ```
+
+## Lookup Mode
+
+Queries for mDNS services on the network. Useful for discovering services advertised by the server mode.
+
+Example usage (to discover the service advertised above):
+
+```
+go run . lookup --serviceType _kcrypt._tcp --interfaceName enp121s0 --timeout 10
+```
+
+or
+
+```
+go run . lookup --serviceType _kcrypt._tcp --timeout 10
+```
+
+The lookup command will output discovered services with their details:
+- Name: The service instance name
+- Host: The hostname of the service
+- Port: The port number
+- AddrV4: The IPv4 address
+- Info: Service information/TXT records
+
+**Note:** The `--interfaceName` flag is optional for lookup. If not specified, the system default multicast interface will be used. The `--timeout` flag defaults to 10 seconds if not specified.
 
 In the context of [kcrypt-challenger](https://github.com/kairos-io/kcrypt-challenger),
 this tool can be used to make a regular kcrypt challenger server be discoverable in
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
@@ -10,11 +9,26 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/hashicorp/mdns"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		runServer()
+		return
+	}
+
+	switch os.Args[1] {
+	case "lookup", "query":
+		runLookup()
+	default:
+		runServer()
+	}
+}
+
+func runServer() {
 	var interfaceName string
 	var address string
 	var serviceType string
@@ -23,12 +37,21 @@ func main() {
 	var err error
 	var ip net.IP
 
-	flag.StringVar(&hostname, "hostName", "", "The hostname that uniquely identifies this instance")
-	flag.StringVar(&interfaceName, "interfaceName", "", "The network interface to expose")
-	flag.StringVar(&address, "address", "", "The IP address to advertise")
-	flag.StringVar(&serviceType, "serviceType", "", "The type to advertise over mdns (e.g. \"_kcrypt._tcp\")")
-	flag.IntVar(&port, "port", 0, "The port to expose")
-	flag.Parse()
+	flagSet := flag.NewFlagSet("server", flag.ExitOnError)
+	flagSet.StringVar(&hostname, "hostName", "", "The hostname that uniquely identifies this instance")
+	flagSet.StringVar(&interfaceName, "interfaceName", "", "The network interface to expose")
+	flagSet.StringVar(&address, "address", "", "The IP address to advertise")
+	flagSet.StringVar(&serviceType, "serviceType", "", "The type to advertise over mdns (e.g. \"_kcrypt._tcp\")")
+	flagSet.IntVar(&port, "port", 0, "The port to expose")
+
+	// Parse flags, handling both cases: with or without subcommand
+	if len(os.Args) > 1 && os.Args[1] != "lookup" && os.Args[1] != "query" {
+		flagSet.Parse(os.Args[1:])
+	} else if len(os.Args) > 2 {
+		flagSet.Parse(os.Args[2:])
+	} else {
+		flagSet.Parse(os.Args[1:])
+	}
 
 	if port == 0 {
 		log.Println("port should be specified with --port")
@@ -54,11 +77,22 @@ func main() {
 		hostname += "."
 	}
 
+	var iface *net.Interface
+
 	if address != "" {
 		ip = net.ParseIP(address)
 		if ip == nil {
 			log.Println("invalid IPv4 address specified")
 			os.Exit(1)
+		}
+		// Find the interface that has this IP address to bind multicast to it
+		iface, err = findInterfaceByIP(ip)
+		if err != nil {
+			log.Printf("Warning: Error finding interface for IP %s: %v. Using system default.", ip, err)
+		} else if iface == nil {
+			log.Printf("Warning: Could not find interface for IP %s. Using system default.", ip)
+		} else {
+			log.Printf("Found interface %s for IP %s", iface.Name, ip)
 		}
 	} else {
 		ip, err = findIPAddress(interfaceName)
@@ -70,21 +104,61 @@ func main() {
 			log.Printf("Could not find an IP address (v4) for interface %s", interfaceName)
 			os.Exit(1)
 		}
+		// Get the interface by name to bind multicast to it
+		iface, err = net.InterfaceByName(interfaceName)
+		if err != nil {
+			log.Printf("Warning: Error finding interface %s: %v. Using system default.", interfaceName, err)
+		} else {
+			log.Printf("Using interface %s", iface.Name)
+		}
 	}
 
 	// Setup our service export
-	host, _ := os.Hostname()
+	// instance: unique name for this service instance
+	// service: service type (e.g., "_kcrypt._tcp")
+	// domain: DNS domain (empty = "local")
+	// hostName: hostname (empty = use system hostname)
+	// port: service port
+	// ips: IP addresses to advertise
+	// txt: TXT record data
+	instanceName := strings.TrimSuffix(hostname, ".")
+
 	info := []string{"An instance of " + serviceType}
-	service, err := mdns.NewMDNSService(host, serviceType, "", hostname, port, []net.IP{ip}, info)
+	service, err := mdns.NewMDNSService(instanceName, serviceType, "", "", port, []net.IP{ip}, info)
 	if err != nil {
-		panic(err.Error())
+		log.Printf("Error creating mDNS service: %v", err)
+		os.Exit(1)
 	}
 
 	// Create the mDNS server, defer shutdown
-	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	// Set Iface if we found one, otherwise let the library choose
+	// NOTE: The hashicorp/mdns library silently ignores errors when binding to multicast.
+	// If binding fails (e.g., port 5353 already in use), the server will be created
+	// but won't actually listen. We enable LogEmptyResponses to help debug.
+	config := &mdns.Config{
+		Zone:              service,
+		LogEmptyResponses: true, // Enable logging to debug queries
+	}
+	if iface != nil {
+		config.Iface = iface
+		log.Printf("Binding mDNS server to interface %s", iface.Name)
+	} else {
+		log.Printf("Using system default multicast interface")
+	}
+	server, err := mdns.NewServer(config)
+	if err != nil {
+		log.Printf("Error creating mDNS server: %v", err)
+		log.Printf("NOTE: If port 5353 is already in use (e.g., by avahi-daemon),")
+		log.Printf("the server may fail to bind. Consider stopping avahi-daemon or")
+		log.Printf("using a different mDNS implementation.")
+		os.Exit(1)
+	}
 	defer server.Shutdown()
 
 	log.Printf("Server created. Advertising %s:%d as %s of type %s", ip, port, hostname, serviceType)
+	log.Printf("Service will respond to queries for: %s", serviceType+".local.")
+	log.Printf("NOTE: The server only responds to queries - it does not proactively announce.")
+	log.Printf("Make sure clients query for: %s", serviceType+".local.")
 	sitAndWait()
 }
 
@@ -93,33 +167,84 @@ func sitAndWait() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	// Create a channel to communicate with the goroutine
-	exitChan := make(chan struct{})
-
-	// Start a goroutine to listen for user input
-	go listenForInput(exitChan)
-
-	// Wait for a signal or user input to exit the program
-	select {
-	case sig := <-signalChan:
-		fmt.Printf("Received signal: %v\n", sig)
-	case <-exitChan:
-		fmt.Println("User pressed a key. Exiting...")
-	}
-
-	// Perform cleanup or additional actions before exiting, if necessary
-	fmt.Println("Program has exited.")
+	// Wait for a signal to exit
+	<-signalChan
+	fmt.Println("Received signal. Shutting down...")
 }
 
-func listenForInput(exitChan chan<- struct{}) {
-	fmt.Print("Press Enter to exit: ")
+func runLookup() {
+	var serviceType string
+	var interfaceName string
+	var timeout int
 
-	// Create a scanner to read a line of input
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
+	flagSet := flag.NewFlagSet("lookup", flag.ExitOnError)
+	flagSet.StringVar(&serviceType, "serviceType", "", "The service type to lookup (e.g., \"_kcrypt._tcp\")")
+	flagSet.StringVar(&interfaceName, "interfaceName", "", "The network interface to use for the lookup")
+	flagSet.IntVar(&timeout, "timeout", 10, "Timeout in seconds for the lookup")
 
-	// Send a signal to the main goroutine to exit
-	exitChan <- struct{}{}
+	if len(os.Args) > 2 {
+		flagSet.Parse(os.Args[2:])
+	} else {
+		flagSet.Parse(os.Args[1:])
+	}
+
+	if serviceType == "" {
+		log.Println("serviceType must be specified with --serviceType")
+		os.Exit(1)
+	}
+
+	var iface *net.Interface
+	var err error
+
+	if interfaceName != "" {
+		iface, err = net.InterfaceByName(interfaceName)
+		if err != nil {
+			log.Printf("Error finding interface %s: %v", interfaceName, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Using interface: %s\n", iface.Name)
+	}
+
+	// Make a channel for results and start listening
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	go func() {
+		for entry := range entriesCh {
+			fmt.Printf("Got new entry:\n")
+			fmt.Printf("  Name: %s\n", entry.Name)
+			fmt.Printf("  Host: %s\n", entry.Host)
+			fmt.Printf("  Port: %d\n", entry.Port)
+			fmt.Printf("  AddrV4: %v\n", entry.AddrV4)
+			fmt.Printf("  AddrV6: %v\n", entry.AddrV6)
+			fmt.Printf("  Info: %s\n", entry.Info)
+			fmt.Printf("  InfoFields: %v\n", entry.InfoFields)
+			fmt.Println()
+		}
+	}()
+
+	fmt.Printf("Looking up service: %s\n", serviceType)
+	fmt.Printf("Timeout: %d seconds\n", timeout)
+	fmt.Println()
+
+	// Use Query with interface if specified
+	params := mdns.DefaultParams(serviceType)
+	if iface != nil {
+		params.Interface = iface
+	}
+	params.Entries = entriesCh
+	params.Timeout = time.Duration(timeout) * time.Second
+	params.DisableIPv6 = true // Disable IPv6 to avoid "network is unreachable" errors
+	err = mdns.Query(params)
+
+	if err != nil {
+		log.Printf("Error during lookup: %v", err)
+		os.Exit(1)
+	}
+
+	// Wait for results
+	time.Sleep(time.Duration(timeout) * time.Second)
+	close(entriesCh)
+
+	fmt.Println("Lookup completed.")
 }
 
 func findIPAddress(iName string) (net.IP, error) {
@@ -144,6 +269,34 @@ func findIPAddress(iName string) (net.IP, error) {
 				if ipv4 := ip.To4(); ipv4 != nil {
 					return ipv4, nil
 				}
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func findInterfaceByIP(targetIP net.IP) (*net.Interface, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range interfaces {
+		iface := &interfaces[i]
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, addr := range addrs {
+			ip, _, err := net.ParseCIDR(addr.String())
+			if err != nil {
+				continue
+			}
+
+			// Check if it's an IPv4 address and matches
+			if ipv4 := ip.To4(); ipv4 != nil && ipv4.Equal(targetIP) {
+				return iface, nil
 			}
 		}
 	}
